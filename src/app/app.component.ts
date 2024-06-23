d6dd70538c53cd0846b7950a69254df8e8ccd796
3liYusuf <alcndb@gmail.com>
2024-06-23 14:31:54 +0300

    experience section semi-done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+  experiences = [
+    {
+      title: 'Software Engineer',
+      from: 'from',
+      to: 'to',
+      at: 'at',
+      description: 'description',
+    },
+    {
+      title: 'DevOps',
+      from: 'from',
+      to: 'to',
+      at: 'at',
+      description: 'description',
+    },
+    {
+      title: 'Full-Stack Developer',
+      from: 'from',
+      to: 'to',
+      at: 'at',
+      description: 'description',
+    },
+  ];
 }
